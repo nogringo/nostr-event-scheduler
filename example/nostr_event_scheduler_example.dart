f7063ae225a2688e263fdc547a53bc30670c0164
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-06-01 15:22:07 +0200

    first commit

---
+++ example/nostr_event_scheduler_example.dart
@@ -0,0 +1,6 @@
+import 'package:nostr_event_scheduler/nostr_event_scheduler.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/nostr_event_scheduler_test.dart
@@ -0,0 +1,16 @@
+import 'package:nostr_event_scheduler/nostr_event_scheduler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
